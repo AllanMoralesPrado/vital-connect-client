58d044c293ab97eb16bb2f4a1e2df8aa1ab5003f
Allan Morales Prado <allan.moralesp@utem.cl>
2024-03-05 17:15:41 -0300

    Componentes ScrollList y Popup en desarrollo

--- vitalconnect/src/App.jsx
+++ vitalconnect/src/App.jsx
@@ -1,6 +1,7 @@
 import NavBar from "./componentes/NavBar";
 import Footer from "./componentes/Footer/footer";
 import CicloVital from "./pages/CicloVital/CicloVital";
+import Hero from "./pages/Hero/Hero";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
 
       <main className="main">
         <CicloVital />
+        {/* <Hero /> */}
       </main>
 
       <Footer />
---
+++ vitalconnect/src/componentes/Popup/Popup.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+
+function Popup() {
+  return (
+    <>
+      {/* coloquen aqui todo lo que quieran */}
+      <h1>Esto por ejemplo</h1>
+    </>
+  );
+}
+
+export default Popup;
