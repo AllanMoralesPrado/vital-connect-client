10e1a3420a5aee02e445de0ee027813d2420052a
jotakf <jpcisternaskf@gmail.com>
2024-03-07 13:01:50 -0300

    asds

--- vitalconnect/src/App.jsx
+++ vitalconnect/src/App.jsx
@@ -5,11 +5,7 @@ import Index from "./pages/Index/Index";
 import Popup from "./componentes/Popup/Popup";
 import Examenes from "./pages/Examenes/Examenes";
 import Localizacion from "./pages/Localizacion/Localizacion";
-<<<<<<< HEAD
 import ScrollList from "./componentes/ScrollList/ScrollList";
-=======
-import Info from "./componentes/Info/Info";
->>>>>>> 21cf5862428e955274330c66b433ba118481cb18
 
 function App() {
   return (
@@ -19,19 +15,12 @@ function App() {
       </header>
 
       <main className="main">
-<<<<<<< HEAD
         {/* <CicloVital /> */}
-        {/* {/* <CicloVital /> */}
-        {/* <Index /> */}
-        {/* <Popup /> */}
-        <ScrollList />
+        <Index />
+        {/* <ScrollList /> */}
         {/* <Popup /> */}
         {/* <Examenes /> */}
         {/* <Localizacion /> */}
-=======
-        <Index />
-        <Popup />
->>>>>>> 21cf5862428e955274330c66b433ba118481cb18
       </main>
 
       <Footer />
